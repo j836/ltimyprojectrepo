ae3a582f88abc070a8a7a9e35c366c82168c42f4
prajwal jain <jainprajwal123@gmail.com>
2021-03-22 06:24:40 -0700

    Called add function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -5,9 +5,10 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Worlddd");
+		add(10,20);
 	}
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
-
+	
 }
