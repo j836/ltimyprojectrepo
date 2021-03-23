e69ef45abf6c39c11b4bcd8eb09bdd99fdc1285d
prajwal jain <jainprajwal123@gmail.com>
2021-03-22 22:48:45 -0700

    Sona nikala

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -28,5 +28,8 @@ public class Calculator {
 	public static void cube(int num1){
 	      System.out.println(num1*num1*num1);
 	  }
+	public static void sona_nikala(){
+	      System.out.println("sona nikala");
+	  }
 	
 }
